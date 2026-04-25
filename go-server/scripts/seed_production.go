d72ac49b8c7c6b6d2658259dfa21e99a2ae3ad15
pushp314 <kamalabai181@gmail.com>
2026-04-25 18:19:48 +0530

    fix: load .env in seeder script

--- go-server/scripts/seed_production.go
+++ go-server/scripts/seed_production.go
@@ -4,12 +4,18 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/pushp314/bizcode/go-server/config"
 	"github.com/pushp314/bizcode/go-server/seeder"
 )
 
 func main() {
 	log.Println("Initializing Production Seeder...")
+
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, relying on environment variables")
+	}
 	
 	// Set environment variables to bypass checks if needed
 	os.Setenv("ENABLE_SEEDER", "true")
